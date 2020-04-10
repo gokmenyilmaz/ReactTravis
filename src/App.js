13ebeb573a42b60d921d8ce5bcd3e3aa3379777e
Gökmen Yılmaz <gokmen1977@gmail.com>
2020-04-11 02:40:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        
-          Travisten Yeni Deploy gh-pages
+          Travisten Githubda değişiklik yaptık ve deploy ettik
       
       </header>
     </div>
