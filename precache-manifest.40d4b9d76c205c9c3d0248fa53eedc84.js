051e274352aca6a118ecbf5d733e63d3ae07ba2c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-10 23:42:11 +0000

    Deploy gokmenyilmaz/ReactTravis to github.com/gokmenyilmaz/ReactTravis.git:gh-pages

---
+++ precache-manifest.40d4b9d76c205c9c3d0248fa53eedc84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953846eb9bc2527d2adfc07935b0cae9",
+    "url": "/ReactTravis/index.html"
+  },
+  {
+    "revision": "e0ee757c2bd09f3124f3",
+    "url": "/ReactTravis/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1bca672dfc8a3d3687f0",
+    "url": "/ReactTravis/static/js/2.a770d47f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTravis/static/js/2.a770d47f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ee757c2bd09f3124f3",
+    "url": "/ReactTravis/static/js/main.076dd7cb.chunk.js"
+  },
+  {
+    "revision": "abafad6b4ea4092dc67a",
+    "url": "/ReactTravis/static/js/runtime-main.49da95ab.js"
+  }
+]);
